6b59a019f2a765b95b72f3dd40c05a1d829be036
hossam khaled <113286871+Hossamkhaled35@users.noreply.github.com>
2022-12-31 02:45:21 +0200

    Add files via upload

---
+++ Webpack/src/hello world.js
@@ -0,0 +1,5 @@
+function hello() {
+ console.log("hello world ^_^");
+    
+}
+export default hello
---
+++ Webpack/src/index.js
@@ -0,0 +1,34 @@
+
+
+
+import hello from "./hello world"
+hello()
+
+
+
+
+
+
+import "./component/header/header"
+import"./component/cover/cover"
+import "./style.css"
+
+import"./component/bio/bio"
+import"./assets/city.png"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //  */
